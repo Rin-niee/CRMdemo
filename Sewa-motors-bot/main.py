fc884a6a276d7f6f3eb0d1693f88cad8627241ab
Rin <she1111eyyyyyy@gmail.com>
2025-08-27 20:47:00 +1000

    дополнение с таймером

--- Sewa-motors-bot/main.py
+++ Sewa-motors-bot/main.py
@@ -99,13 +99,20 @@ async def main():
             Бесконечный цикл для отправки напоминаний
             Проверяет заказы каждую минуту и отправляет уведомления
             """
+            timer = 0
             while True:
                 try:
                     await reminder_job(bot)
-                    await reminder_open_bids(bot)
+                    if timer >= 900:
+                        logger.info("15 минут прошли — запускаем reminder_open_bids")
+                        await reminder_open_bids(bot)
+                        timer = 0
+                    else:
+                        logger.info(f"Таймер: {timer} секунд")
                 except Exception:
                     pass
                 await asyncio.sleep(10)
+                timer +=10
 
         # Запускаем фоновую задачу для напоминаний
         asyncio.create_task(reminders_loop())
